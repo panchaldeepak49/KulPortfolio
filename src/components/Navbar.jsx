786ebb385c018a2e652cf3c47d1217ac0e652271
Deepak <panchaldeepak49@gmail.com>
2024-11-01 19:03:03 +0530

    new code push,scrolling updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     window.scrollTo({ top: 1930, behavior : "smooth" })
    }
    const goToBtnSkillsMb = ()=>{
-    window.scrollTo({ top: 2710, behavior : "smooth" })
+    window.scrollTo({ top: 2265, behavior : "smooth" })
    }
 
 
@@ -39,7 +39,7 @@ const Navbar = () => {
     window.scrollTo({ top: 2605, behavior : "smooth" })
    }
    const goToBtnProjectMb = ()=>{
-    window.scrollTo({ top: 3440, behavior : "smooth" })
+    window.scrollTo({ top: 2930, behavior : "smooth" })
    }
 
    const goToBtnContact = ()=>{
@@ -55,7 +55,7 @@ const Navbar = () => {
     <>
      <div className='sticky top-0 w-full h-20 bg-[#496989] flex justify-between items-center'>
         {/* <img src={''} className='md:h-19 sm:h-16 h-12 sm:ml-20 ml-5 cursor-pointer' ></img> */}
-        <p className='ml-5 sm:ml-10 font-semibold text-[1.25rem] sm:text-[2rem] text-white hover:text-orange-500 cursor-pointer transition-all ease-in duration-200' onClick={goToBtnHome}>KPortfolio</p>
+        <p className='ml-5 sm:ml-10 font-semibold text-[1.25rem] sm:text-[2rem] text-white hover:text-orange-400 cursor-pointer transition-all ease-in duration-200' onClick={goToBtnHome}>KPortfolio</p>
 
      <nav>
      <span className='text-3xl max-[400px]:text-2xl sm:hidden block cursor-pointer ml-20 mr-4 mt-40' onClick={toggleMenu}>
@@ -65,7 +65,7 @@ const Navbar = () => {
      <ul className={` md:flex md:items-center md:justify-between relative  md:mr-6  z-50  md:static md:mt-0 bg-[#496989] sm:mt-96  md:h-20 h-auto md:w-auto 
                     left-0 md:py-0 py-4 md:pl-0 sm:opacity-100 ${isMainMenuOpen ? 'opacity-100' : 'opacity-0'}  `}>
 
-        <li className='flex items-center md:mx-2 md:py-8 py-2 px-2 font-Syne text-white cursor-pointer sm:text-xl text-xs hover:text-green-500 transition-all ease-in duration-100'>
+        <li className='flex items-center md:mx-2 md:py-8 py-2 px-2 font-Syne text-white cursor-pointer sm:text-xl text-xs hover:text-orange-400 transition-all ease-in duration-100'>
             {/* <Link to='/about'>About</Link>  */}
             {/* <p onClick={goToBtnAbout}>About</p> */}
             {window.innerWidth > 600 ? (
@@ -94,7 +94,7 @@ const Navbar = () => {
                             )}
         </li>
 
-        <li className='flex items-center md:mx-2 md:py-8 py-2 px-2 font-Syne text-white cursor-pointer sm:text-xl text-xs hover:text-orange-500 transition-all ease-in duration-100'>
+        <li className='flex items-center md:mx-2 md:py-8 py-2 px-2 font-Syne text-white cursor-pointer sm:text-xl text-xs hover:text-orange-400 transition-all ease-in duration-100'>
             {/* <Link to='/project' >Project</Link>  */}
             {/* <p onClick={goToBtnProject}>Project</p> */}
             {window.innerWidth > 600 ? (
